014c0ecd223d9b64bacd3abb4a7ec392cd8cb6e1
Saoa35 <saoamah@gmail.com>
2022-09-11 15:44:54 +0200

    + handleFocus method

--- src/Components/RefsEx/RefsEx.js
+++ src/Components/RefsEx/RefsEx.js
@@ -8,14 +8,20 @@ export default class RefsEx extends React.Component {
         this.myRef = React.createRef();
         this.inputRef = React.createRef();
         this.handleChange = this.handleChange.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
     }
 
     componentDidMount() {
         console.log(this.myRef.current.textContent);
+        this.inputRef.current.focus();
     }
 
     handleChange() {
         console.log(this.inputRef.current.value);
+    }
+
+    handleFocus() {
+        this.inputRef.current.focus();
     }
 
     render() {
@@ -29,6 +35,8 @@ export default class RefsEx extends React.Component {
                 </h2>
 
                 <input type='text' ref={this.inputRef} onChange={this.handleChange}/>
+
+                <button onClick={this.handleFocus}>Fokus on input</button>
             </div>
         )
     }
