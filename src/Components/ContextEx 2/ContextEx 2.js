18e13dbf9e61ad236883cc28b035ea1cc01d8ed6
Saoa35 <saoamah@gmail.com>
2022-09-12 19:51:31 +0200

    added ContextEx2 to ContextEx

--- src/Components/ContextEx 2/ContextEx 2.js
+++ src/Components/ContextEx 2/ContextEx 2.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { MyContext } from "../../Context-api/myContext";
 
-export default class ContextEx extends React.Component {
+export default class ContextEx2 extends React.Component {
 
     render() {
 
         return(
             <div>
                 <h3>
-                ContextEx
+                ContextEx 2
                 </h3>
                 
-                <MyContext.Consumer>
-                    {(value) => {
-                        console.log(value);
-                    return <span>context content!</span>
-                }}   
-                </MyContext.Consumer>
                 
             </div>
         )
--- src/Components/ContextEx/ContextEx.js
+++ src/Components/ContextEx/ContextEx.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MyContext } from "../../Context-api/myContext";
+import ContextEx2 from "../ContextEx 2/ContextEx 2";
 
 export default class ContextEx extends React.Component {
 
@@ -10,13 +11,15 @@ export default class ContextEx extends React.Component {
                 <h3>
                 ContextEx
                 </h3>
-                
+
                 <MyContext.Consumer>
                     {(value) => {
                         console.log(value);
                     return <span>context content!</span>
                 }}   
                 </MyContext.Consumer>
+
+                <ContextEx2/>
                 
             </div>
         )
