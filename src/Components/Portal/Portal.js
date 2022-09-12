57054f7cb5acf1214aa24ad77576f5139408a19c
Saoa35 <saoamah@gmail.com>
2022-09-12 21:02:40 +0200

    created modal div

--- src/Components/Portal/Portal.js
+++ src/Components/Portal/Portal.js
@@ -1,31 +1,16 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 class Modal extends React.Component {
     constructor(props) {
       super(props);
-      this.el = document.createElement('div');
-    }
-  
-    componentDidMount() {
-      // Элемент портала добавляется в DOM-дерево после того, как
-      // потомки компонента Modal будут смонтированы, это значит,
-      // что потомки будут монтироваться на неприсоединённом DOM-узле.
-      // Если дочерний компонент должен быть присоединён к DOM-дереву
-      // сразу при подключении, например, для замеров DOM-узла,
-      // или вызова в потомке 'autoFocus', добавьте в компонент Modal
-      // состояние и рендерите потомков только тогда, когда
-      // компонент Modal уже вставлен в DOM-дерево.
-      modalRoot.appendChild(this.el);
-    }
-  
-    componentWillUnmount() {
-      modalRoot.removeChild(this.el);
+      this.modal = document.getElementById('modal');
     }
   
     render() {
       return ReactDOM.createPortal(
         this.props.children,
-        this.el
+        this.modal
       );
     }
   }
